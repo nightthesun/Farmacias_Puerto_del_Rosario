6f4b597a836f180bfe975fe96ecc16932dac21e7
nightthesun <remberto-cori-alejo@outlook.com>
2024-03-27 18:32:26 -0400

    feat:directorio

---
+++ app/Http/Controllers/DirClienteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dir_Cliente;
+use Illuminate\Http\Request;
+
+class DirClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dir_Cliente $dir_Cliente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dir_Cliente $dir_Cliente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dir_Cliente $dir_Cliente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dir_Cliente $dir_Cliente)
+    {
+        //
+    }
+}
